dfaf4cee3aeda19216d2ece796e4b5c9f75f2c1b
Hydraxst <matthieu.fayol-daude@gmail.com>
2025-01-23 22:51:19 +0100

    modif index.html réglage des image et redirections

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const movieId = urlParams.get("id");
-const APIKEY = "7c8d496a";
-
 fetch(`https://www.omdbapi.com/?i=tt15467380&apikey=7c8d496a&plot=full`)
   .then((response) => response.json())
   .then((data) => {
-    const moviePoster = document.getElementById("mov-img-2");
+    const moviePoster = document.getElementById("mov1-1");
     moviePoster.innerHTML = `
-            <a id="pour-img"><img id="img-mov-1" src="${data.Poster}" alt=""></a>
-            <p id="text-top">${data.Poster}</p>
+            <a href="movie.html?id=tt15467380"><img id="img-mov-1" src="${data.Poster}"></img></a>
+            <p id="text-top">${data.Title}</p>
+            `;
+  })
+  .catch((error) => {
+    console.error("Erreur lors de la récupération des données:", error);
+  });
+  fetch(`https://www.omdbapi.com/?i=tt13622970&apikey=7c8d496a&plot=full`)
+  .then((response) => response.json())
+  .then((data) => {
+    const moviePoster = document.getElementById("mov1-2");
+    moviePoster.innerHTML = `
+            <a href="movie.html?id=tt13622970"><img id="img-mov-1" src="${data.Poster}"></img></a>
+            <p id="text-top">${data.Title}</p>
+            `;
+  })
+  .catch((error) => {
+    console.error("Erreur lors de la récupération des données:", error);
+  });
+  fetch(`https://www.omdbapi.com/?i=tt2975590&apikey=7c8d496a&plot=full`)
+  .then((response) => response.json())
+  .then((data) => {
+    const moviePoster = document.getElementById("mov1-3");
+    moviePoster.innerHTML = `
+            <a href="movie.html?id=tt2975590"><img id="img-mov-1" src="${data.Poster}"></img></a>
+            <p id="text-top">${data.Title}</p>
             `;
   })
   .catch((error) => {
