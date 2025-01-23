8e96680063c4c6eb9e757c41d2f8a2525b6a566f
Hydraxst <matthieu.fayol-daude@gmail.com>
2025-01-23 23:32:05 +0100

    update d'un film sorti en 2024

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ fetch(`https://www.omdbapi.com/?i=tt15467380&apikey=7c8d496a&plot=full`)
   .catch((error) => {
     console.error("Erreur lors de la récupération des données:", error);
   });
-  fetch(`https://www.omdbapi.com/?i=tt2975590&apikey=7c8d496a&plot=full`)
+  fetch(`https://www.omdbapi.com/?i=tt12037194&apikey=7c8d496a&plot=full`)
   .then((response) => response.json())
   .then((data) => {
     const moviePoster = document.getElementById("mov2-3");
     moviePoster.innerHTML = `
-            <a href="movie.html?id=tt2975590"><img id="img-mov-1" src="${data.Poster}"></img></a>
+            <a href="movie.html?id=tt12037194"><img id="img-mov-1" src="${data.Poster}"></img></a>
             <p id="text-top">${data.Title}</p>
             `;
   })
