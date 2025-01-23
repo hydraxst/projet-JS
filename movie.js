7c948fef7e76e731851799bbae1a1f137f1897c5
Hydraxst <matthieu.fayol-daude@gmail.com>
2025-01-23 21:03:14 +0100

    finalisation movies.html

--- movie.js
+++ movie.js
@@ -2,26 +2,24 @@ const urlParams = new URLSearchParams(window.location.search);
 const movieId = urlParams.get('id')
 const APIKEY = '7c8d496a'
 if (movieId) {
-    fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=${APIKEY}`)
+    fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=7c8d496a&plot=full`)
+        .then(response => response.json())
         .then(data => {
-            if (data.Response === "True") {
-                const movieDetails = document.getElementById('movieDetails');
-                movieDetails.innerHTML = `
-                    <img src="${data.Poster}" alt="${data.Title}">
-                    <h2>${data.Title}</h2>
-                    <p>Année: ${data.Year}</p>
-                    <p>Genre: ${data.Genre}</p>
-                    <p>Réalisateur: ${data.Director}</p>
-                    <p>Acteurs: ${data.Actors}</p>
-                    <p>Synopsis: ${data.Plot}</p>
-                `;
-            } else {
-                document.getElementById('movieDetails').innerHTML = '<p>Film non trouvé.</p>';
-            }
+            const movieDetails = document.getElementById('container-mov');
+            movieDetails.innerHTML = `
+                <h1 id="title-mov">${data.Title}</h1>
+                <img id="img-mov" src="${data.Poster}" alt="${data.Title}">
+                <h3 id="title-resume">Résumé</h3>
+                <p id="word-resume">Résumé: ${data.Plot}</p>
+                <h3 id="title-genre">Genre</h3>
+                <p id="word-genre">Genre: ${data.Genre}</p>
+                <h3 id="title-actor">Actor</h3>
+                <p id="word-actor">Acteurs: ${data.Actors}</p>
+            `;
         })
         .catch(error => {
             console.error('Erreur lors de la récupération des données:', error);
         });
 } else {
-    document.getElementById('movieDetails').innerHTML = '<p>Aucun film sélectionné.</p>';
+    document.getElementById('container-mov').innerHTML = '<p>Aucun film sélectionné.</p>';
 }
